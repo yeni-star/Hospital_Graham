e72cd0a71e9f5989ee09212ac653b092e90f9ada
yeniky <k.dreams17@gmail.com>
2026-06-07 17:15:56 -0400

    primer commit bff

---
+++ backend/bff-hospital-graham/src/main/java/cl/duoc/bff_hospital_graham/BffHospitalGrahamApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.bff_hospital_graham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffHospitalGrahamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffHospitalGrahamApplication.class, args);
+	}
+
+}
