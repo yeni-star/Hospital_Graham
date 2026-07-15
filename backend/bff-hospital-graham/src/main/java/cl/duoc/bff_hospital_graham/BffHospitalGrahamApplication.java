6790a038b36e2cf2c3b6dac10ab9fdd97ad1be1f
Nicolas <142134618+NICOLASGUOR@users.noreply.github.com>
2026-07-14 21:27:05 -0400

    docker-compose

--- backend/bff-hospital-graham/src/main/java/cl/duoc/bff_hospital_graham/BffHospitalGrahamApplication.java
+++ backend/bff-hospital-graham/src/main/java/cl/duoc/bff_hospital_graham/BffHospitalGrahamApplication.java
@@ -2,10 +2,12 @@ package cl.duoc.bff_hospital_graham;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BffHospitalGrahamApplication {
 
+	@Bean
 	public static void main(String[] args) {
 		SpringApplication.run(BffHospitalGrahamApplication.class, args);
 	}
