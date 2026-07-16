ae179d1a6ec0ff1ff6b18ae6c60b52f38b689c85
Nicolas <142134618+NICOLASGUOR@users.noreply.github.com>
2026-07-15 21:02:12 -0400

    d

--- backend/bff-hospital-graham/src/main/java/cl/duoc/bff_hospital_graham/BffHospitalGrahamApplication.java
+++ backend/bff-hospital-graham/src/main/java/cl/duoc/bff_hospital_graham/BffHospitalGrahamApplication.java
@@ -3,13 +3,19 @@ package cl.duoc.bff_hospital_graham;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BffHospitalGrahamApplication {
 
-	@Bean
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BffHospitalGrahamApplication.class, args);
 	}
+
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
